51bdd02ee57cb58b8e08d967a3876f8f03bb1c7d
diorithaxhiolli <104323157+diorithaxhiolli@users.noreply.github.com>
2024-01-15 00:36:10 +0100

    Account+Job views and controllers

---
+++ Controllers/JobController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JAS.Controllers
+{
+    public class JobController : Controller
+    {
+        public IActionResult JobApplication()
+        {
+            return View();
+        }
+    
+        public IActionResult JobCreation()
+        {
+            return View();
+        }
+    
+    }
+}
